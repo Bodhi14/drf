efc4878c7206e32d5a59caee7b151175f8f711c2
Bodhi14 <bodhi140802@gmail.com>
2022-05-03 17:39:50 +0530

    drf project

---
+++ proj/blog/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+class Article(models.Model):
+    author = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    date = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=100)
+    
+
+    def __str__(self):
+        return self.title
+
+
+
+    
+        
+
+
+
+
+# Create your models here.
---
+++ proj/blog/views.py
@@ -0,0 +1,87 @@
+from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from rest_framework.parsers import JSONParser
+from django.contrib import admin
+
+from .models import Article
+from .serializers import ArticleSerializer
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework import status
+
+from rest_framework import permissions
+
+@api_view(['GET', 'POST'])
+@permission_classes((permissions.AllowAny,))
+def article_list(request):
+    if request.method == 'GET':
+        articles = Article.objects.all()
+        serializer = ArticleSerializer(articles, many=True)
+        return JsonResponse(serializer.data, safe = False)
+
+    elif request.method == 'POST':
+        serializer = ArticleSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+        
+
+def article_detail(request, pk):
+    try:
+        article = Article.objects.get(pk=pk)
+
+    except Article.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        serializer = ArticleSerializer(article)
+        return JsonResponse(serializer.data)
+    
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = ArticleSerializer(data=data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+    
+    elif request.method == 'DELETE':
+        article.delete()
+        return HttpResponse(status=204)
+        
+    
+
+        
+
+
+
+
+    
+    
+
+
+
+    
+
+
+        
+
+
+
+
+
+        
+
+
+
+
+
+
+
+
+
+# Create your views here.
